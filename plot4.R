07e8c69207cd3e35a8538e923539dcc89d13477c
John Stevenson <jackcs99@gmail.com>
2015-08-06 11:28:13 -0400

    added comments to code

--- plot4.R
+++ plot4.R
@@ -1,9 +1,16 @@
+# for dmy and hms functions
 library(lubridate)
+
 x <- read.table("household_power_consumption.txt",sep=";",header=TRUE)
+
+#unified datetime
 y<- dmy(x$Date)+hms(x$Time)
+
+#select only dates required
 z <- y>= ymd("2007-02-01") & y<ymd("2007-02-03")
 subX <- x[z,]
 subY <- y[z]
+
 png("plot4.png")
 par(mfrow = c(2,2))
 plot(subY,as.numeric(as.character(subX$Global_a)),type="l",ylab="Global Active Power",xlab="")
