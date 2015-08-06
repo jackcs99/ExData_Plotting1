956ce82fb3e55742d2f46e77fa74fca34057c0fc
John Stevenson <jackcs99@gmail.com>
2015-08-06 10:31:41 -0400

    remove kilowat uints from axis title

--- plot4.R
+++ plot4.R
@@ -6,8 +6,7 @@ subX <- x[z,]
 subY <- y[z]
 png("plot4.png")
 par(mfrow = c(2,2))
-plot(subY,as.numeric(as.character(subX$Global_a)),type="l",ylab="Global Active Power
-",xlab="")
+plot(subY,as.numeric(as.character(subX$Global_a)),type="l",ylab="Global Active Power",xlab="")
 plot(subY,as.numeric(as.character(subX$Vo)),type="l",ylab="Voltage",xlab="datetime")
 plot(subY,as.numeric(as.character(subX$Sub_metering_1)),type="l",ylab="Energy sub metering",xlab="")
 lines(subY,as.numeric(as.character(subX$Sub_metering_2)),type="l",col="red")
