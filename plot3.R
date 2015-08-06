42891d8421b45976d8315d5e52d41c45388131ce
John Stevenson <jackcs99@gmail.com>
2015-08-06 11:20:24 -0400

    iterate to change commit comment

--- plot3.R
+++ plot3.R
@@ -1,12 +1,19 @@
+# for dmy and hms functions
 library(lubridate)
+
 x <- read.table("household_power_consumption.txt",sep=";",header=TRUE)
+
+#unified datetime
 y<- dmy(x$Date)+hms(x$Time)
+
+#select only dates required
 z <- y>= ymd("2007-02-01") & y<ymd("2007-02-03")
 subX <- x[z,]
 subY <- y[z]
+
 png("plot3.png")
 plot(subY,as.numeric(as.character(subX$Sub_metering_1)),type="l",ylab="Energy sub metering",xlab="")
 lines(subY,as.numeric(as.character(subX$Sub_metering_2)),type="l",col="red")
 lines(subY,as.numeric(as.character(subX$Sub_metering_3)),type="l",col="blue")
-legend("topright",lty=1,col=c("black","red","blue"),c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+legend("topright",lty=1,col=c("black","red","blue"),c("Sub_metering 1","Sub_metering_2","Sub_metering_3"))
 dev.off()
